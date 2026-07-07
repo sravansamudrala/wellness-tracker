18c79e630f300be9922ffc4b4695a4e7a5b6611a
sravansamudrala <62881303+sravansamudrala@users.noreply.github.com>
2026-07-07 15:57:47 +0530

    added skincare staticpage

--- src/pages/Skincare.tsx
+++ src/pages/Skincare.tsx
@@ -1,5 +1,29 @@
 function Skincare() {
-    return <h2>Skincare</h2>;
+  return (
+    <div>
+      <h2>🧴 Skincare</h2>
+
+      <h3>Today's Progress</h3>
+
+      <p>0%</p>
+
+      <h3>Morning Routine</h3>
+
+      <ul>
+        <li>☐ Face Wash</li>
+        <li>☐ Vitamin C</li>
+        <li>☐ Moisturizer</li>
+        <li>☐ Sunscreen</li>
+      </ul>
+
+      <h3>Evening Routine</h3>
+
+      <ul>
+        <li>☐ Cleanser</li>
+        <li>☐ Moisturizer</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Skincare;
