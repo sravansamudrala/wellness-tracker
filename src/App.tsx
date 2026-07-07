f133b0609d9427e2ddb675446f10d5154490a7ba
sravansamudrala <sravankumar.s@thoughtworks.com>
2026-07-07 12:43:36 +0530

    test iphone

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>Wellness Tracker</h1>
-      <p>My first React application 🚀</p>
+      <p>My first React application  , Hi Sravan🚀</p>
     </div>
   );
 }
