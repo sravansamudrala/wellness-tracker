a957036032d3cbdc04e6cff67e0cb5861ae78501
sravansamudrala <sravankumar.s@thoughtworks.com>
2026-07-09 12:52:14 +0530

    added pwa plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: "autoUpdate",
+      manifest: {
+        name: "AI Wellness Tracker",
+        short_name: "Wellness",
+        description: "AI-powered Wellness Tracker",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
+});
